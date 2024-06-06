763e5dc7e11689a1701e83ea242bc66ab076e512
Rizki Ikhwan <rizkiikhwan73@gmail.com>
2024-06-06 11:34:11 +0700

    edit requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-import os
-import tensorflow as tf
-import numpy as np
 from flask import Flask, render_template, request, redirect, url_for
 
-
-model = tf.keras.models.load_model('models/modelBrainTumorMRI.h5')
 
 app = Flask(__name__)
 
